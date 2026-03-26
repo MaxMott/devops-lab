94357dd3d6257a1e2e57cf5cef2b41116b95b929
Max Mott <maxmottershead@icloud.com>
2026-03-26 17:11:11 +0000

    adding insecure true@

--- k8s-resources/main.tf
+++ k8s-resources/main.tf
@@ -12,12 +12,12 @@ resource "kubernetes_namespace_v1" "harness_ns" {
 
 resource "kubernetes_resource_quota_v1" "mem_limit" {
   metadata {
-    name = "mem-quota"
+    name      = "mem-quota"
     namespace = kubernetes_namespace_v1.harness_ns.metadata[0].name
   }
   spec {
     hard = {
-        memory = "1Gi"
+      memory = "1Gi"
     }
   }
 }
