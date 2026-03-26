b2941b6b31591c4c55d7326c024bff76357b192c
Max Mott <maxmottershead@icloud.com>
2026-03-26 16:17:30 +0000

    adding variables

--- k8s-resources/providers.tf
+++ k8s-resources/providers.tf
@@ -17,7 +17,10 @@ terraform {
 provider "kubernetes" {
   host = "https://192.168.49.2:8443"
 
-  client_certificate     = base64decode(var.kubernetes_client_cert)
-  client_key             = base64decode(var.kubernetes_client_key)
-  cluster_ca_certificate = base64decode(var.kubernetes_ca_cert)
+  # client_certificate     = base64decode(var.kubernetes_client_cert)
+  # client_key             = base64decode(var.kubernetes_client_key)
+  # cluster_ca_certificate = base64decode(var.kubernetes_ca_cert)
+  client_certificate     = var.kubernetes_client_cert
+  client_key             = var.kubernetes_client_key
+  cluster_ca_certificate = var.kubernetes_ca_cert
 }
